1922fee512483d282bc8b4232839772557ac67f7
Fehmin1905 <fehminqurbanli2@gmail.com>
2022-04-02 23:33:37 +0400

    Add project files.

---
+++ CarCompany.API.UI/Controllers/CarController.cs
@@ -0,0 +1,66 @@
+﻿using CarCompany.DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarCompany.API.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        public string filename = @"carDb.txt";
+
+
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            StreamWriter sw = new StreamWriter(filename,true);
+            sw.WriteLine(car.Id+"-"+car.Brand + "-" + car.Model + "-" + car.Color + "-" + car.Year);
+            sw.Close();
+            return Ok();
+        }
+
+        [HttpGet("GetCars")]
+        public IActionResult GetCars()
+        {
+            List<string> list = new List<string>();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCarById(int id)
+        {
+            string car=null;
+            
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(id.ToString()==line.Split('-').First())
+                    {
+                        car = line;
+                        break;
+                    }
+                }
+            }
+            return Ok(car);
+        }
+
+
+    }
+}
---
+++ CarCompany.DataAccess/Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCompany.DataAccess.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
